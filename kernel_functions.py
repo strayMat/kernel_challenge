cd774cb88fb0d5c6c70b0bf98b9005815d7dcb04
strayMat <mat.dout@hotmail.fr>
2018-02-20 17:56:53 +0100

    adding utils files for preprocessing and kernels functions and my_exploration notebook

---
+++ kernel_functions.py
@@ -0,0 +1,54 @@
+# Kernel functions
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+from itertools import product
+from string import ascii_lowercase
+
+'''
+Compute the gram matrix of a reproducing kernel K(x, y) and data X even if the embedding phi is unknown
+input: 	X as array
+		rk a python function computing K(x, y) (function from X^2 -> R)
+output: [K(x_i, y_j)] = <phi(x_i), phi(x_j)> for i,j in 1..n
+'''
+def gram_rk(X, rk):
+	n = X.shape[0]
+	K = np.zeros((n, n))
+	for i in tqdm(range(n)):
+		for j in range(n):
+			K[i, j] = rk(X[i], X[j])
+	return K
+
+'''
+Compute the gram matrix of a kernel given the embedding phi of this kernel
+input: 	X as array
+		phi a python function computing phi(x) (from X to R^d where d is the dimension of the embedding)
+output: [K(x_i, y_j)] = <phi(x_i), phi(x_j)> for i,j in 1..n
+'''
+def gram_phi(X, phi):
+	n = X.shape[0]
+	X_feat = np.array([phi(x) for x in X])
+	K = X_feat.dot(X_feat.T)
+	return K
+
+# k_spectrum kernel
+## Creation of all possible combinations
+base_azote = ['A', 'C', 'T', 'G']
+dic_prot = [''.join(i) for i in product(base_azote, repeat = 3)]
+nb_feat = len(dic_prot)
+
+# k_spectrum kernel with k = 3
+## Creation of all possible combinations
+base_azote = ['A', 'C', 'T', 'G']
+dic_aa = [''.join(i) for i in product(base_azote, repeat = 3)]
+nb_feat = len(dic_aa)
+
+def count_kuplet(seq):
+    k_grams_count = np.zeros(nb_feat)
+    for i,e in enumerate(dic_aa):
+        k_grams_count[i] = seq.count(e)
+    return k_grams_count
+
---
+++ preprocessing.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def preprocessing(X, Y, percent=0.8):
+    """
+    Preprocessing the data.(expect as input numpy arrays)
+        - Shuffle
+        - Divide data and labels
+        - centering
+        - add dimension to the data
+        - cut into a training dataset and a test dataset
+    """
+
+    # Shuffle phase
+    np.random.RandomState(1)
+    rand_ix = np.arange(X.shape[0])
+    np.random.shuffle(rand_ix)
+    n_training = int(percent  * Y.shape[0])
+    train_ix = rand_ix[:n_training]
+    test_ix = rand_ix[n_training:]
+    ## only for numerical data (does not work with sequences)
+    # centering data
+    # X = X-np.mean(X, axis=0)
+
+    # Scaling data
+    # X = scale( X, axis=0, with_mean=True, with_std=True, copy=True )
+
+    # Add one dimension to your data points in order to account for
+    # the offset if your data is not centered.
+    # X = np.concatenate((X, np.ones((X.shape[0], 1))), axis=1)
+
+    # Compute the training and the test set
+    X_train = X[train_ix]
+    Y_train = Y[train_ix]
+    X_test = X[test_ix]
+    Y_test = Y[test_ix]
+
+    return X_train, Y_train, X_test, Y_test
